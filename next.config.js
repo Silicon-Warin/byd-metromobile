31f835d453e48ca84cd20ca268bc7a036ed3fd63
mezealot24 <wiriya.on@gmail.com>
2025-03-19 19:59:15 +0700

    check secure

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
+const crypto = require("crypto");
+
 module.exports = {
 	async headers() {
+		const nonce = crypto.randomBytes(16).toString("base64");
 		return [
 			{
 				source: "/(.*)",
 				headers: [
 					{
 						key: "Content-Security-Policy",
-						value:
-							"default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self'; object-src 'none'; base-uri 'self'; frame-src 'self' https:;",
+						value: `
+						  default-src 'self';
+						  script-src 'self' 'nonce-${nonce}';
+						  style-src 'self' 'nonce-${nonce}';
+						  img-src 'self' data: https:;
+						  font-src 'self' https://fonts.gstatic.com;
+						  object-src 'none';
+						  base-uri 'self';
+						  frame-src 'self' https:;
+						  report-uri /csp-report;
+						`,
 					},
 					{
 						key: "Strict-Transport-Security",
@@ -24,5 +36,15 @@ module.exports = {
 				],
 			},
 		];
+	},
+	images: {
+		domains: ["bydmetromobile.com"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "bydmetromobile.com",
+				pathname: "/**",
+			},
+		],
 	},
 };
