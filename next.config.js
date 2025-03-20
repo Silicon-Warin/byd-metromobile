7bb7a4e745136cdb9356d4114943c53de27fa764
mezealot24 <wiriya.on@gmail.com>
2025-03-20 11:48:02 +0700

    setup Next Image

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "bydmetromobile.com",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "images.unsplash.com",
-				pathname: "/**",
-			},
-		],
+	experimental: {
+		optimizeCss: true,
 	},
 };
 
