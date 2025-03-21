9cbcc56aba964be7623080c1ee36e96ba8dfa633
mezealot24 <wiriya.on@gmail.com>
2025-03-22 04:52:30 +0700

    fix casesensitive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,9 +5,8 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Phone, Mail, MapPin, Clock, Send } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
-import { branches, socialMedia, contactInfo } from "@/data/contactData";
 import { SocialIcon } from "@/components/ui/SocialIcon";
-
+import { branches, socialMedia, contactInfo } from "@/data/contactdata";
 export default function ContactPage() {
 	// Animation variants
 	const fadeIn = {
